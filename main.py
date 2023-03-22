f88ccbd12440b0a0d18f9c9b4b1963109d975016
Kevin <kevschool1179@gmail.com>
2023-03-22 15:27:05 -0400

    Made it so file opens up web browser with Tetris automatically. Redid input system so it uses a dictionary instead for easier modification and addition of inputs. Added hard drop input and changed the down input.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import pyautogui
 import pydirectinput
+import webbrowser
 
 #COMMAND SETUP
 # Down = [1,1,1,1,1]
@@ -15,6 +16,21 @@ detector = HandDetector(detectionCon = 0.8, maxHands=1)
 video = cv2.VideoCapture(0)
 
 framesBetweenInput = 20
+webbrowser.open_new_tab('https://tetris.com/play-tetris')
+#window = webview.create_window("Tetris", 'https://tetris.com/play-tetris', width = 1000, height = 750) 
+#webview.start()
+
+inputs = {
+    #Note: changed the gesture for moving piece down
+    tuple([1,0,0,0,0]) : ('STATE: DOWN', 'down'),
+    tuple([1,1,0,0,0]) : ('STATE: ROTATE LEFT', 'z'),
+    tuple([1,0,0,0,1]) : ('STATE: ROTATE RIGHT', 'x'),
+    tuple([0,0,0,0,0]) : ('STATE: IDLE', ''),
+    tuple([0,1,0,0,0]) : ('STATE: LEFT', 'left'),
+    tuple([0,0,0,0,1]) : ('STATE: RIGHT', 'right'),
+    #I felt holding all fingers up is more natural for hard drop, if you disagree feel free to change the gestures
+    tuple([1,1,1,1,1]) : ('STATE: HARD DROP', 'space'),
+}
 
 while True:
     ret, frame = video.read()
@@ -25,23 +41,13 @@ while True:
         lmlist = hands[0]
         fingerUp = detector.fingersUp(lmlist)
         print(fingerUp);
-        if fingerUp==[1,1,1,1,1]:
-            cv2.putText(frame, 'STATE: DOWN', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            pydirectinput.press('down')
-        if fingerUp==[0,0,0,0,0]:
-            cv2.putText(frame, 'STATE: IDLE', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-        if fingerUp==[1,1,0,0,0]:
-            cv2.putText(frame, 'STATE: ROTATE LEFT', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            pydirectinput.press('z')
-        if fingerUp==[1,0,0,0,1]:
-            cv2.putText(frame, 'STATE: ROTATE RIGHT', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            pydirectinput.press('up')
-        if fingerUp==[0,0,0,0,1]:
-            cv2.putText(frame, 'STATE: RIGHT', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            pydirectinput.press('right')
-        if fingerUp==[0,1,0,0,0]:
-            cv2.putText(frame, 'STATE: LEFT', (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
-            pydirectinput.press('left')
+        currentInput = inputs.get(tuple(fingerUp))
+
+        if currentInput != None:
+            cv2.putText(frame, currentInput[0], (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+            pydirectinput.press(currentInput[1])
+
+
     cv2.imshow("Frame", frame)
     #this is to quit out of the video frame
     k = cv2.waitKey(1)

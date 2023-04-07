15c1a90a16110ad7467ea1bd5308d8ebb6fd8e73
Kevin <kevschool1179@gmail.com>
2023-04-07 10:14:50 -0400

    Reverted back to commit used in demo.

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from time import time
-
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import pyautogui
 import pydirectinput
 import webbrowser
 
-# COMMAND SETUP
+#COMMAND SETUP
 # Down = [1,1,1,1,1]
 # Rotate Left = [1,1,0,0,0]
 # Rotate Right = [1,0,0,0,1]
@@ -13,57 +12,44 @@ import webbrowser
 # Right = [0,1,0,0,0]
 # Left= [0,0,0,0,1]
 
-detector = HandDetector(detectionCon=0.8, maxHands=2)
+detector = HandDetector(detectionCon = 0.8, maxHands=1)
 video = cv2.VideoCapture(0)
 
 framesBetweenInput = 20
-# webbrowser.open_new_tab('https://tetris.com/play-tetris')
-# window = webview.create_window("Tetris", 'https://tetris.com/play-tetris', width = 1000, height = 750)
-# webview.start()
+webbrowser.open_new_tab('https://tetris.com/play-tetris')
+#window = webview.create_window("Tetris", 'https://tetris.com/play-tetris', width = 1000, height = 750) 
+#webview.start()
 
-left = {
-    (0, 1, 0, 0, 1): ('STATE: ROTATE LEFT', 'z'),
-    (1, 1, 0, 0, 0): ('STATE: LEFT', 'left'),
-    (1, 0, 0, 0, 0): ('STATE: DOWN', 'down'),
+inputs = {
+    #Note: changed the gesture for moving piece down
+    tuple([1,0,0,0,0]) : ('STATE: DOWN', 'down'),
+    tuple([1,1,0,0,0]) : ('STATE: ROTATE LEFT', 'z'),
+    tuple([1,0,0,0,1]) : ('STATE: ROTATE RIGHT', 'x'),
+    tuple([0,0,0,0,0]) : ('STATE: IDLE', ''),
+    tuple([0,1,0,0,0]) : ('STATE: LEFT', 'left'),
+    tuple([0,0,0,0,1]) : ('STATE: RIGHT', 'right'),
+    #I felt holding all fingers up is more natural for hard drop, if you disagree feel free to change the gestures
+    tuple([1,1,1,1,1]) : ('STATE: HARD DROP', 'space'),
 }
-right = {
-    (0, 1, 0, 0, 0): ('STATE: ROTATE RIGHT', 'x'),
-    (1, 1, 0, 0, 0): ('STATE: RIGHT', 'right'),
-    (1, 1, 1, 1, 1): ('STATE: HARD DROP', 'space'),
-}
-
-def getInput(hands):
-    rightHand = tuple(detector.fingersUp(hands[0]))[1:]
-    leftHand = tuple(detector.fingersUp(hands[1]))[1:]
-
-    currentInput = left.get(leftHand)
-    if currentInput is None:
-        currentInput = right.get(rightHand)
-
-    if currentInput is None:
-        currentInput = ("State: Idle", '')
-
-    return currentInput
-
-
-secSinceLastInput = 0
-currentInput = ("", "")
 
 while True:
     ret, frame = video.read()
     hands, image = detector.findHands(frame)
+    framesBetweenInput -= 1
+    if hands and framesBetweenInput <= 0:
+        framesBetweenInput = 10
+        lmlist = hands[0]
+        fingerUp = detector.fingersUp(lmlist)
+        print(fingerUp);
+        currentInput = inputs.get(tuple(fingerUp))
 
-    if len(hands) == 2 and time() > secSinceLastInput + .5:
-        secSinceLastInput = time()
+        if currentInput != None:
+            cv2.putText(frame, currentInput[0], (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
+            pydirectinput.press(currentInput[1])
 
-        currentInput = getInput(hands)
 
-        pydirectinput.press(currentInput[1])
-
-    cv2.putText(frame, currentInput[0], (20, 460), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA)
     cv2.imshow("Frame", frame)
-
-    # this is to quit out of the video frame
+    #this is to quit out of the video frame
     k = cv2.waitKey(1)
     if k == ord('q'):
         break
